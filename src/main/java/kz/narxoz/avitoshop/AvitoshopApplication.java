640eded2f38773a82fd0232074acb4bdf1895142
aigerimbakh <milashkalovec@gmail.com>
2022-11-26 18:24:13 +0300

    git init

---
+++ src/main/java/kz/narxoz/avitoshop/AvitoshopApplication.java
@@ -0,0 +1,13 @@
+package kz.narxoz.avitoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvitoshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvitoshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/narxoz/avitoshop/AvitoshopApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.avitoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvitoshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
