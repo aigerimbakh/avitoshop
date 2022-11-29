b19b7948a4d5880a67c84258e68284749965d108
aigerimbakh <milashkalovec@gmail.com>
2022-11-30 01:42:00 +0300

    all f

--- src/main/java/kz/narxoz/avitoshop/model/Product.java
+++ src/main/java/kz/narxoz/avitoshop/model/Product.java
@@ -5,26 +5,44 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "products")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
- @Id
- @GeneratedValue(strategy = GenerationType.AUTO)
- @Column(name = "id")
-   private Long id;
- @Column(name = "title")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title")
     private String title;
- @Column(name = "description",columnDefinition = "text")
+    @Column(name = "description", columnDefinition = "text")
     private String description;
- @Column(name = "price")
+    @Column(name = "price")
     private int price;
- @Column(name = "city")
+    @Column(name = "city")
     private String city;
- @Column(name = "author")
+    @Column(name = "author")
     private String author;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,
+            mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init() {
+        dateOfCreated = LocalDateTime.now();
+    }
 
 
+    public void addImageToProduct(Image image) {
+        image.setProduct(this);
+        images.add(image);
+    }
 }
